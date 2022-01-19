c957116772ec5ae7545e5156256522b3053c4f86
jonas-94 <jonas_2010@hotmail.com>
2022-01-19 10:35:30 +0100

    ny

--- NikeClientApp/NikeClientApp/Views/MapPage.xaml.cs
+++ NikeClientApp/NikeClientApp/Views/MapPage.xaml.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace NikeClientApp.UWP.Views
+namespace NikeClientApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class RegisterPage : ContentPage
+    public partial class MapPage : ContentPage
     {
-        public RegisterPage()
+        public MapPage()
         {
             InitializeComponent();
         }
