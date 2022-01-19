dffb6cd14aac6d4a90271c2c5ba53f58b64f57a0
Samson <31880780+Zerueru90@users.noreply.github.com>
2022-01-19 16:15:53 +0100

    LogInPage entre, knappar och designen rätt klar, saknas runda kanter på entre boxen, färgen är inte helt rätt och bakgrundsbilden sitter inte som den ska.

--- NikeClientApp/NikeClientApp/MainPage.xaml.cs
+++ NikeClientApp/NikeClientApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NikeClientApp.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,6 +14,20 @@ namespace NikeClientApp
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void BtnLogIn_Clicked(object sender, EventArgs e)
+        {
+            //Vertification that the user exist, if true than send to next page, if no write error message.
+
+
+
+            await Navigation.PushAsync(new MapPage());
+        }
+
+        private async void BtnRegister_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegisterPage());
         }
     }
 }
