af9ffb131c97f8303c671da76ad037085ea16306
jonas-94 <jonas_2010@hotmail.com>
2022-01-20 17:46:06 +0100

    karta mer klar

--- NikeClientApp/NikeClientApp/Views/MapPage.xaml.cs
+++ NikeClientApp/NikeClientApp/Views/MapPage.xaml.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
+using Xamarin.Forms.Maps;
 namespace NikeClientApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +15,29 @@ namespace NikeClientApp.Views
         public MapPage()
         {
             InitializeComponent();
+            
+        }
+       
+       
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            Geocoder geoCoder = new Geocoder();
+
+            IEnumerable<Position> approximateLocations = await geoCoder.GetPositionsForAddressAsync(TBSearchbar.Text);
+            Position position = approximateLocations.FirstOrDefault();
+            string coordinates = $"{position.Latitude}, {position.Longitude}";
+            CityName.Text = TBSearchbar.Text;
+            
+            MapSpan maps = new MapSpan(position, 1.10, 0.10);
+            Mapsample.MoveToRegion(maps);
+
+
+        }
+
+        private void ImageButton_Clicked_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
