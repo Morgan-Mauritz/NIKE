56106a4f6a2671c259e9f016798b23cf74c35e3c
rocknrollaswe <d.inghamn@gmail.com>
2022-01-27 15:29:04 +0100

    httpServices for Registration/Login

---
+++ NikeClientApp/NikeClientApp/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NikeClientApp.Models
+{
+    public class User
+    {
+        public string Firstname { get; set; }
+       
+        public string Lastname { get; set; }
+       
+        public string Email { get; set; }
+        
+        public string Password { get; set; }
+        
+        public string Username { get; set; }
+        public string ApiKey { get; set; }
+    }
+}
---
+++ NikeClientApp/NikeClientApp/Models/UserApi.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NikeClientApp.Models
+{
+    public static class UserApi
+    {
+        public static string ApiKey { get; set; }
+    }
+}
