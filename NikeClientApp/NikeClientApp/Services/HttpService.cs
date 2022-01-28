cf30205ef4a4873da8bd4e9dfae12885249a8722
Samson Zeru <samsonzeru@hotmail.com>
2022-01-28 14:26:36 +0100

    Skapat HttpService metoder

--- NikeClientApp/NikeClientApp/Services/HttpService.cs
+++ NikeClientApp/NikeClientApp/Services/HttpService.cs
@@ -4,10 +4,11 @@ using System.Text;
 using RestSharp;
 using System.Threading;
 using System.Threading.Tasks;
+using NikeClientApp.Models;
 
 namespace NikeClientApp.Services
 {
-    public class HttpService<T,K>
+    public class HttpService<T> where T : class
     {
         private readonly RestSharp.RestClient _restClient; 
 
@@ -16,12 +17,46 @@ namespace NikeClientApp.Services
             _restClient = new RestClient("https://localhost:44393/");
         }
         
-        public async Task<K> Post(T obj, string endPoint)
+        public async Task<Response<T>> Post(string endPoint, T obj)
         {
             var request = new RestRequest(endPoint);
             request.Method = Method.Post;
             request.AddBody(obj);
-            return await _restClient.PostAsync<K>(request);
-        }      
+            //request.AddHeader("apiKey", UserApi.ApiKey);
+            return await _restClient.PostAsync<Response<T>>(request);
+        }
+        
+        public async Task<PaginationResponse<List<T>>> GetList(string endPoint, string query)
+        {
+            var request = new RestRequest(endPoint + query);
+            request.Method = Method.Get;
+            request.AddHeader("apiKey", UserApi.ApiKey);
+            return await _restClient.GetAsync<PaginationResponse<List<T>>>(request);
+        }
+
+        public async Task<Response<T>> Get(string endPoint, string query)
+        {
+            var request = new RestRequest(endPoint + query);
+            request.Method = Method.Get;
+            request.AddHeader("apiKey", UserApi.ApiKey);
+            return await _restClient.GetAsync<Response<T>>(request);
+        }
+
+        public async Task<Response<T>> Update(string endPoint, T obj)
+        {
+            var request = new RestRequest(endPoint);
+            request.Method = Method.Put;
+            request.AddBody(obj);
+            request.AddHeader("apiKey", UserApi.ApiKey);
+            return await _restClient.PutAsync<Response<T>>(request);
+        }
+
+        public async Task<Response<T>> Delete(string endPoint)
+        {
+            var request = new RestRequest(endPoint);
+            request.Method = Method.Delete;
+            request.AddHeader("apiKey", UserApi.ApiKey);
+            return await _restClient.DeleteAsync<Response<T>>(request);
+        }
     }
 }
