aa523725be7c4efb52801cba1f3d35edee6dbfda
Samson Zeru <samsonzeru@hotmail.com>
2022-02-14 12:01:30 +0100

    skapat metod för offline inlogg och settar true eller false för UserOffLine.LoggedIn

--- NikeClientApp/NikeClientApp/ViewModels/MainPageViewModel.cs
+++ NikeClientApp/NikeClientApp/ViewModels/MainPageViewModel.cs
@@ -26,35 +26,44 @@ namespace NikeClientApp.ViewModels
 
         public Command LogIn => new Command(async () => await OnLogIn());
         public Command RegisterPage => new Command(async () => await NavigationService.NavigateTo<RegisterPageViewModel>());
-        public Command MapPage => new Command(async () => await NavigationService.NavigateTo<MapPageViewModel>());
+        public Command MapPage => new Command(async () => await OffLineLogIn());
         public async Task OnLogIn()
         {
             try
             {
-            if (string.IsNullOrEmpty(User.Email))
-            {
-                await Application.Current.MainPage.DisplayAlert("Felmeddelande", "Du måste fylla i alla fält", "OK");
-                return;
-            }
-            if (!Regex.IsMatch(User.Email, @"^[a-zåäöA-ZÅÄÖ][\w\.-]*[a-zåäöA-ZÅÄÖ0-9]@[a-zåäöA-ZÅÄÖ0-9][\w\.-]*[a-zåäöA-ZÅÄÖ0-9]\.[a-zåäöA-ZÅÄÖ][a-zåäöA-ZÅÄÖ\.]*[a-zåäöA-ZÅÄÖ]$"))
-            {
-                await Application.Current.MainPage.DisplayAlert("Felmeddelande", "Ej korrekt email", "OK");
-                return;
-            }
-                User.Password = Encrypt.EncryptMessage(User.PasswordText);
-            var responseData = await userClient.Post("authorization/login", User, false);
-            UserApi.ApiKey = responseData.Data.ApiKey;
+                if (string.IsNullOrEmpty(User.Email))
+                {
+                    await Application.Current.MainPage.DisplayAlert("Felmeddelande", "Du måste fylla i alla fält", "OK");
+                    return;
+                }
+                if (!Regex.IsMatch(User.Email, 
+                    @"^[a-zåäöA-ZÅÄÖ][\w\.-]*[a-zåäöA-ZÅÄÖ0-9]@[a-zåäöA-ZÅÄÖ0-9][\w\.-]*[a-zåäöA-ZÅÄÖ0-9]\.[a-zåäöA-ZÅÄÖ][a-zåäöA-ZÅÄÖ\.]*[a-zåäöA-ZÅÄÖ]$"))
+                {
+                    await Application.Current.MainPage.DisplayAlert("Felmeddelande", "Ej korrekt email", "OK");
+                    return;
+                }
+                    UserOffLine.LoggedIn = true;
+                    User.Password = Encrypt.EncryptMessage(User.PasswordText);
+                    var responseData = await userClient.Post("authorization/login", User, false);
+                    UserApi.ApiKey = responseData.Data.ApiKey;
+                    await NavigationService.NavigateTo<MapPageViewModel>();
             }
             catch (HttpRequestException)
             {
                 await Application.Current.MainPage.DisplayAlert("Felmeddelande", "Fel inloggningsuppgifter", "OK");
                 return;
             }
+        }
 
-
-            //TODO: Change to mappage
-            await NavigationService.NavigateTo<MapPageViewModel>();
-
+        public async Task OffLineLogIn()
+        {
+            var ans = await App.Current.MainPage.DisplayAlert("Varning", "För att kunna lägga till sevärdheter, kommentera och betygsätta, \nmåste du först registrera dig.", "OK", "Avbryt");
+            if (ans == true)
+            {
+                UserOffLine.LoggedIn = false;
+                //TODO: Change to mappage
+                await NavigationService.NavigateTo<MapPageViewModel>();
+            }
         }
 
         public MainPageViewModel(INaviService naviService) : base(naviService)
